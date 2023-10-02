84f094d78a009ed36c1a1c6b5e5c2a40770420e9
Justus von Voß <108137850+Hookyhook@users.noreply.github.com>
2023-10-02 08:17:23 +0000

    Added new providers

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,21 @@
 import type { NextAuthOptions } from "next-auth";
+
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Discord from "next-auth/providers/discord";
+
 import Credentials from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Discord({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
     Github({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
