9ab9dcc416d8cbeacfbaf9e40fc1e15524cd8004
Justus von Voß <108137850+Hookyhook@users.noreply.github.com>
2023-10-12 07:28:19 +0000

    Added redirect Made Navbar faster

--- app/components/molecules/Navbar.tsx
+++ app/components/molecules/Navbar.tsx
@@ -1,58 +1,42 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { Session, getServerSession } from "next-auth";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
 
-import { signOut, useSession } from "next-auth/react";
-
-export default function Navbar() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  const redirectToHome = (): void => {
-    router.replace("/");
-  };
-
-  const redirectToCompany = (): void => {
-    router.replace("/company");
-  };
-
-  const redirectToProfile = (): void => {
-    router.replace("/profile");
-  };
+export default async function Navbar() {
+  const session: Session | null = await getServerSession();
 
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a
-          className="btn btn-ghost normal-case text-xl"
-          onClick={redirectToHome}
-        >
+        <Link className="btn btn-ghost normal-case text-xl" href={"/"}>
           adPlace
-        </a>
+        </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a className="text-xl" onClick={redirectToCompany}>
+            <Link className="text-xl" href={"/company"}>
               Company Area
-            </a>
+            </Link>
           </li>
         </ul>
-        {status === "authenticated" && (
+        {session && (
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src={session.user?.image!} />
+                <img src={session.user!.image!} />
               </div>
             </label>
             <ul
               tabIndex={0}
               className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
             >
-              <li onClick={() => redirectToProfile()}>
-                <a>Profile</a>
+              <li>
+                <Link href={"/profile"}>Profile</Link>
               </li>
-              <li onClick={() => signOut()}>
+              <li onClick={() => signOut({ redirect: true, callbackUrl: "/" })}>
                 <a>Logout</a>
               </li>
             </ul>
